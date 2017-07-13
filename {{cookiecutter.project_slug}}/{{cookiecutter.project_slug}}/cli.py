27de63c3549e2e495311b11d94c6f775fc77b0c5
Kevin Wierman <kwierman@gmail.com>
2017-07-13 12:56:09 -0500

    Update cli.py

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/cli.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/cli.py
@@ -2,12 +2,12 @@
 
 import click
 import logging
-import sys
 
 
 @click.command()
 def main(files=None):
     logging.basicConfig(level=logging.INFO)
 
+    
 if __name__ == "__main__":
     main()
